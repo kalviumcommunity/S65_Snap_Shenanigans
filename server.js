04b875464b27075501f42b26018f9af8ff856280
Monish892 <jayachitraramesh08@gmail.com>
2025-01-29 14:03:53 +0530

    Added MongoDB connection and DB status in home route

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
-const express=require('express');
-const app=express();
-const PORT=process.env.PORT || 3000;
+// const express=require('express');
+// const app=express();
+// const PORT=process.env.PORT || 3000;
 
-app.get('/ping',(req,res)=>{
-    res.send('Hello World');
-});
+// app.get('/ping',(req,res)=>{
+//     res.send('Hello world');
+// });
 
-app.listen(PORT,()=>{
+// app.listen(PORT,()=>{
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const Mongo_url = process.env.Mongo_url;
+
+mongoose.connect(Mongo_url)
+.then(()=>{
+    console.log('Connected to the database');
+}).catch(err=>{
+    console.log("Mongodb failed",err);
+})
+
+
+app.get('/ping', (req, res) => {
+
+    const dbstatus=mongo.connection.readyState===1?'Connected':'Not Connected';
+    res.json({ message: 'Hello World', dbstatus: dbstatus });
+
+})
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+})
